8244fcd6fa12a9297d3cb180e65a4e535c49f1ba
AkselLyk-Dania <akly5@student.eadania.dk>
2026-03-24 08:36:21 +0100

    Commit

---
+++ QuestWorld/QuestWorldMainWPF/Interfaces/BaseClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestWorldMainWPF.Interfaces
+{
+    public interface IBasePClass
+    {
+        /*
+        public void Flee()
+        {
+
+        }
+
+        public void UseItem()
+        {
+
+        }
+        */
+    }
+
+    public interface IGuard //Brug defense
+    {
+        /*
+        public void Guard()
+        {
+
+        }
+        */
+    }
+
+    public interface IMelee //Våben som ikke er skyts
+    {
+        /*
+        public void MeleeAttack()
+        {
+
+        }
+        */
+    }
+
+    public interface IRange //Skyts
+    {
+        /*
+        public void RangedAttack()
+        {
+
+        }
+        */
+    }
+
+    public interface IMagic //Magi
+    {
+        /*
+        public void MagicAttack()
+        {
+
+        }
+        */
+    }
+
+    public interface ISpeech //Karisma
+    {
+        /*
+        public void Persuade()
+        {
+
+        }
+        */
+    }
+}
